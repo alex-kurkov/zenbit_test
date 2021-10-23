f6aaae8ebba3d18f3fb452aac619d96ca5f1fece
alex-kurkov <alexkourkov@yandex.ru>
2021-10-23 19:47:04 +0500

    add cors options

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,12 @@ import { ValidateInputPipe } from './core/pipes/validate.pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidateInputPipe());
-  app.enableCors()
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  })
   await app.listen(3000);
 }
 bootstrap();
