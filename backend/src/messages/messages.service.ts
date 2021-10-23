39e45ec9729ae34a47a1ca8106d895de4a9b2f65
alex-kurkov <alexkourkov@yandex.ru>
2021-10-23 20:18:01 +0500

    change body handling

--- backend/src/messages/messages.service.ts
+++ backend/src/messages/messages.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, Body } from '@nestjs/common';
 import { MessageEnt } from './message.entity';
 import { MESSAGE_REPOSITORY } from 'src/core/constants';
 
@@ -9,7 +9,8 @@ export class MessagesService {
     private readonly messageRepository: typeof MessageEnt,
   ) {}
 
-  async create(data): Promise<MessageEnt> {
+  async create(@Body() data): Promise<MessageEnt> {
+    console.log(data);
     return await this.messageRepository.create({ ...data });
   }
   async findAll(): Promise<MessageEnt[]> {
